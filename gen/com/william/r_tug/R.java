9a81083fddc783d216998dabac466d7999ce7aef
Brian <bellbrian14@gmail.com>
2013-10-01 23:07:58 -0500

    BioHarness Demo Code

--- gen/com/william/r_tug/R.java
+++ gen/com/william/r_tug/R.java
@@ -24,12 +24,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int ButtonConnect=0x7f080004;
+        public static final int ButtonDisconnect=0x7f080005;
+        public static final int HRTextBox=0x7f080007;
+        public static final int PeakAcc=0x7f08000e;
+        public static final int Posture=0x7f08000c;
+        public static final int RespirationRate=0x7f080008;
+        public static final int SkinTemperature=0x7f08000a;
+        public static final int StatusTextBox=0x7f080010;
+        public static final int Welcometext=0x7f080003;
+        public static final int action_settings=0x7f080012;
+        public static final int initiate_bioharness_button=0x7f080002;
+        public static final int labelHeartRate=0x7f080006;
+        public static final int labelPeakAcc=0x7f08000f;
+        public static final int labelPosture=0x7f08000d;
+        public static final int labelRespRate=0x7f080009;
+        public static final int labelSkinTemp=0x7f08000b;
+        public static final int labelStatusMsg=0x7f080011;
         public static final int start_button=0x7f080001;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int bioharness=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -37,7 +54,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int hello=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int labelHeartRate=0x7f050005;
+        public static final int labelPeakAcc=0x7f050009;
+        public static final int labelPosture=0x7f050008;
+        public static final int labelRespRate=0x7f050006;
+        public static final int labelSkinTemp=0x7f050007;
+        public static final int labelStatusMsg=0x7f05000a;
         public static final int start_button=0x7f050003;
     }
     public static final class style {
--- src/com/william/r_tug/MainActivity.java
+++ src/com/william/r_tug/MainActivity.java
@@ -1,15 +1,33 @@
 package com.william.r_tug;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button go_to_bioharness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        this.go_to_bioharness = (Button) this.findViewById(R.id.initiate_bioharness_button);
+		
+		this.go_to_bioharness.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent bioHarness = new Intent(getApplicationContext(),
+						BioHarness.class);
+
+				startActivity(bioHarness);
+			}
+		});
+
     }
 
 
